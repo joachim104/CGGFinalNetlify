0e9f81cae62c05f43bf07e20fd9393e780eae776
joachim104 <joachim.stougaard@hotmail.com>
2019-03-23 16:05:45 +0100

    lavet en main.js fil

--- main.js
+++ main.js
@@ -53,8 +53,6 @@ if(!document.getElementById('id8')) {
     var link = document.createElement('link');
     link.id = 'id8';
     link.rel = 'stylesheet';
-    link.integrity = 'sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm';
-    link.crossOrigin = "anonymous"
     link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css';
     document.head.appendChild(link);
 }
