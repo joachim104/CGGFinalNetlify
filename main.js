19ccf47581c37502691408256694360127bdc919
joachim104 <joachim.stougaard@hotmail.com>
2019-03-23 17:30:56 +0100

    main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-if (!document.getElementById('id5')) {
+if (!document.getElementById('id1')) {
     var link = document.createElement('link');
-    link.id = 'id5';
+    link.id = 'id1';
     link.rel = 'stylesheet';
     link.integrity = 'sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm';
     link.crossOrigin = "anonymous"
@@ -21,23 +21,16 @@ if (!document.getElementById('id3')) {
     link.href = 'https://fonts.googleapis.com/css?family=Old+Standard+TT|Yatra+One';
     document.head.appendChild(link);
 }
-// if (!document.getElementById('id4')) {
-//     var link = document.createElement('link');
-//     link.id = 'id4';
-//     link.rel = 'stylesheet';
-//     link.href = 'styles/footer.css';
-//     document.head.appendChild(link);
-// }
-if (!document.getElementById('id6')) {
+if (!document.getElementById('id4')) {
     var link = document.createElement('link');
-    link.id = 'id6';
+    link.id = 'id4';
     link.rel = 'stylesheet';
     link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css';
     document.head.appendChild(link);
 }
-if (!document.getElementById('id7')) {
+if (!document.getElementById('id5')) {
     var link = document.createElement('link');
-    link.id = 'id7';
+    link.id = 'id5';
     link.rel = 'icon';
     link.href = 'assets/LogoGG_2018_2_actualized.png';
     document.head.appendChild(link);
