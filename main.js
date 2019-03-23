26a1c7a574c35322fe36d4eaf88f1060f08ebb89
joachim104 <joachim.stougaard@hotmail.com>
2019-03-23 16:03:12 +0100

    lavet en main.js fil

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 
-if(!document.getElementById('id1')) {
-    var script = document.createElement('script');
-    script.id = 'id1';
-    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
-    document.head.appendChild(script);
-}
-if(!document.getElementById('id2')) {
-    var script = document.createElement('script');
-    script.id = 'id2';
-    script.src = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js';
-    document.head.appendChild(script);
-}
+// if(!document.getElementById('id1')) {
+//     var script = document.createElement('script');
+//     script.id = 'id1';
+//     script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
+//     script.defer = true;
+//     document.head.appendChild(script);
+// }
+// if(!document.getElementById('id2')) {
+//     var script = document.createElement('script');
+//     script.id = 'id2';
+//     script.src = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js';
+//     document.head.appendChild(script);
+// }
 if(!document.getElementById('id3')) {
     var link = document.createElement('link');
     link.id = 'id3';
@@ -37,6 +38,24 @@ if(!document.getElementById('id6')) {
     link.id = 'id6';
     link.rel = 'stylesheet';
     link.href = 'styles/footer.css';
+    document.head.appendChild(link);
+}
+if(!document.getElementById('id7')) {
+    var link = document.createElement('link');
+    link.id = 'id7';
+    link.rel = 'stylesheet';
+    link.integrity = 'sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm';
+    link.crossOrigin = "anonymous"
+    link.href = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css';
+    document.head.appendChild(link);
+}
+if(!document.getElementById('id8')) {
+    var link = document.createElement('link');
+    link.id = 'id8';
+    link.rel = 'stylesheet';
+    link.integrity = 'sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm';
+    link.crossOrigin = "anonymous"
+    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css';
     document.head.appendChild(link);
 }
 
