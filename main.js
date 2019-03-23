cdaec6678228014c17d1256535fd027797f0dbc7
joachim104 <joachim.stougaard@hotmail.com>
2019-03-23 16:10:47 +0100

    lavet en main.js fil

--- main.js
+++ main.js
@@ -1,59 +1,52 @@
-
-// if(!document.getElementById('id1')) {
-//     var script = document.createElement('script');
-//     script.id = 'id1';
-//     script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
-//     script.defer = true;
-//     document.head.appendChild(script);
-// }
-// if(!document.getElementById('id2')) {
-//     var script = document.createElement('script');
-//     script.id = 'id2';
-//     script.src = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js';
-//     document.head.appendChild(script);
-// }
+if(!document.getElementById('id1')) {
+    var link = document.createElement('link');
+    link.id = 'id1';
+    link.rel = 'stylesheet';
+    link.href = 'styles/style.css';
+    document.head.appendChild(link);
+}
+if(!document.getElementById('id2')) {
+    var link = document.createElement('link');
+    link.id = 'id2';
+    link.rel = 'stylesheet';
+    link.href = 'https://fonts.googleapis.com/css?family=Roboto:300';
+    document.head.appendChild(link);
+}
 if(!document.getElementById('id3')) {
     var link = document.createElement('link');
     link.id = 'id3';
     link.rel = 'stylesheet';
-    link.href = 'https://fonts.googleapis.com/css?family=Roboto:300';
+    link.href = 'https://fonts.googleapis.com/css?family=Old+Standard+TT|Yatra+One';
     document.head.appendChild(link);
 }
 if(!document.getElementById('id4')) {
     var link = document.createElement('link');
     link.id = 'id4';
     link.rel = 'stylesheet';
-    link.href = 'https://fonts.googleapis.com/css?family=Old+Standard+TT|Yatra+One';
+    link.href = 'styles/footer.css';
     document.head.appendChild(link);
 }
 if(!document.getElementById('id5')) {
     var link = document.createElement('link');
     link.id = 'id5';
     link.rel = 'stylesheet';
-    link.href = 'styles/style.css';
+    link.integrity = 'sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm';
+    link.crossOrigin = "anonymous"
+    link.href = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css';
     document.head.appendChild(link);
 }
 if(!document.getElementById('id6')) {
     var link = document.createElement('link');
     link.id = 'id6';
     link.rel = 'stylesheet';
-    link.href = 'styles/footer.css';
+    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css';
     document.head.appendChild(link);
 }
 if(!document.getElementById('id7')) {
     var link = document.createElement('link');
     link.id = 'id7';
-    link.rel = 'stylesheet';
-    link.integrity = 'sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm';
-    link.crossOrigin = "anonymous"
-    link.href = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css';
-    document.head.appendChild(link);
-}
-if(!document.getElementById('id8')) {
-    var link = document.createElement('link');
-    link.id = 'id8';
-    link.rel = 'stylesheet';
-    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css';
+    link.rel = 'icon';
+    link.href = 'assets/LogoGG_2018_2_actualized.png';
     document.head.appendChild(link);
 }
 
